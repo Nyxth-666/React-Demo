cad9ba40c58a2f76c419e6b6b0e138ca46b86aef
Uriyann <joshuabarotea06@gmail.com>
2026-05-01 20:12:53 +0800

    done with api integration, design later

---
+++ Week7-9_Integrating_API/src/App.jsx
@@ -0,0 +1,49 @@
+import { useEffect, useState } from "react";
+
+function App() {
+  const [users, setUsers] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchUsers = async () => {
+      try {
+        const response = await fetch(
+          "https://jsonplaceholder.typicode.com/users",
+        );
+
+        if (!response.ok) {
+          throw new Error("Failed to fetch data");
+        }
+
+        const data = await response.json();
+        setUsers(data);
+        setLoading(false);
+      } catch (err) {
+        setError(err.message);
+        setLoading(false);
+      }
+    };
+
+    fetchUsers();
+  }, []);
+
+  if (loading) return <h2>Loading...</h2>;
+  if (error) return <h2>Error: {error}</h2>;
+
+  return (
+    <>
+      <h1>User List</h1>
+
+      {users.map((user) => (
+        <div key={user.id}>
+          <h3>{user.name}</h3>
+          <p>Email: {user.email}</p>
+          <p>Company: {user.company.name}</p>
+        </div>
+      ))}
+    </>
+  );
+}
+
+export default App;
